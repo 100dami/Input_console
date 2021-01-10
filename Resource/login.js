5ac557866b7e6b9b5470f66fc05942ccb34a2884
dami <s20033@gsm.hs.kr>
2021-01-10 22:41:45 +0900

    [DOCS] Change file structure

---
+++ Resource/login.js
@@ -0,0 +1,12 @@
+document.getElementById("output").onclick = function output() {
+    
+    let id = document.getElementById("id").value;
+    let pw = document.getElementById("pw").value;
+
+    console.log("id : " + id);
+    console.log("pw : " + pw);
+}
+
+
+
+
